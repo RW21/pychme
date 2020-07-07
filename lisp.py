2afdc932932ff94f3e127e6994d1b2fd4178aee5
RW21 <rinze.watanabe.translation@gmail.com>
2020-07-07 13:04:14 +1000

    Update

--- lisp.py
+++ lisp.py
@@ -1,32 +1,60 @@
-import re
-from collections import deque
+import operator
+
+
+class Environment:
+    def __init__(self):
+        self.keywords = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            # '/': operator.di
+        }
+
+
+special_symbols = {'+', '/', '-', '*'}
 
 
 def parse(p) -> list:
     p = p.replace('(', ' ( ').replace(')', ' ) ').split()
     p.reverse()
-    print(p)
+    return tokenize(p)
+
+
+def tokenize(p):
     token = p.pop()
+
     if token == '(':
-        tokens = []
+        p_ = []
         while p[-1] != ')':
-            token = p.pop()
-            tokens.append(parse(token))
+            p_.append(tokenize(p))
         p.pop()
-        return tokens
-    elif token == ')':
-        raise SyntaxError
+        return p_
     else:
         if token[0].isalpha():
+            return token
+        elif token in special_symbols:
             return token
         else:
             return eval(token)
 
-# def evaluate(t):
 
-class Environment:
-   def __init__(self):
-       self.keywords = {}
+environment = Environment()
 
 
-print(parse('(f (f 1 1) 10)'))
+def evaluate(t):
+    if type(t) == int or type(t) == float:
+        return t
+    else:
+        procedure = environment.keywords[t[0]]
+        args = [evaluate(k) for k in t[1:]]
+        print(procedure)
+        print(args)
+        return procedure(*args)
+
+
+a = '(+ 1 (+ 123 2))'
+p = '(define (name a b) (+ a b )'
+
+parsed = parse(a)
+print(parsed)
+print(evaluate(parsed))

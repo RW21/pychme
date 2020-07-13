9c0effae362b534f500dc8549caf1ada70ce2e9a
Rinze Watanabe <rinze.watanabe.translation@gmail.com>
2020-07-13 23:16:23 +1000

    Update

--- lisp.py
+++ lisp.py
@@ -1,5 +1,20 @@
 import operator
+import os
 
+os.system("")
+
+# Group of Different functions for different styles
+class style():
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
 
 class Environment:
     def __init__(self):
@@ -52,7 +67,7 @@ def evaluate(t):
     elif t[0] == 'define':
         environment.keywords[t[1]] = evaluate(t[2])
     elif t[0] == 'lambda':
-
+        pass
     else:
         procedure = environment.keywords[t[0]]
         args = [evaluate(k) for k in t[1:]]
@@ -70,7 +85,7 @@ print(evaluate(parsed))
 
 def repl():
     while True:
-        parsed = parse(input('scheme> '))
+        parsed = parse(input(style.MAGENTA + 'scheme> '))
         print(evaluate(parsed))
 
 repl()

b5b5f544a806052cf2d27c7656dca18f75a5ccde
RW21 <rinze.watanabe.translation@gmail.com>
2020-07-14 21:47:22 +1000

    Update

--- lisp.py
+++ lisp.py
@@ -1,33 +1,21 @@
 import operator
-import os
+import sys
 
-os.system("")
-
-# Group of Different functions for different styles
-class style():
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
 
 class Environment:
-    def __init__(self):
-        self.keywords = {
-            '+': operator.add,
-            '-': operator.sub,
-            '*': operator.mul,
-            'atom?': (lambda a : type(a) != list),
-            # '/': operator.di
-        }
+    def __init__(self, params=[], args=[], env=None):
+        self.keywords = {}
+        self.keywords.update(zip(params, args))
 
 
-special_symbols = {'+', '/', '-', '*'}
+primitives = {
+    '+': operator.add,
+    '-': operator.sub,
+    'atom?': lambda a: type(a) != list,
+    'car': lambda a: a[0],
+    'cdr': lambda a: a[1:],
+    'cons': lambda a, b: [a] + b
+}
 
 
 def parse(p) -> list:
@@ -48,44 +36,50 @@ def tokenize(p):
     else:
         if token[0].isalpha():
             return token
-        elif token in special_symbols:
+        elif token in primitives:
             return token
         else:
             return eval(token)
 
 
-environment = Environment()
+class Procedure:
+    def __init__(self, params, body, env):
+        self.params, self.body, self.env = params, body, env
+
+    def __call__(self, *args, **kwargs):
+        return evaluate(self.body, Environment(self.params, args))
 
 
-def evaluate(t):
-    print(t)
+def evaluate(t, environment):
     if type(t) == int or type(t) == float or (type(t) == str and t not in environment.keywords):
         return t
     elif type(t) == str and t in environment.keywords:
         # variable reference
         return environment.keywords[t]
     elif t[0] == 'define':
-        environment.keywords[t[1]] = evaluate(t[2])
+        environment.keywords[t[1]] = evaluate(t[2], environment)
     elif t[0] == 'lambda':
-        pass
+        params, body = t[1], t[2]
+        return Procedure(params, body, environment)
     else:
-        procedure = environment.keywords[t[0]]
-        args = [evaluate(k) for k in t[1:]]
-        print(procedure)
-        print(args)
+        procedure = evaluate(t[0], environment)
+        args = [evaluate(k, environment) for k in t[1:]]
         return procedure(*args)
 
 
-a = '(+ 1 (+ 123 2))'
-p = '(define name (+ 1 1 ))'
+standard_env = Environment(params=primitives.keys(), args=primitives.values())
 
-parsed = parse(p)
-print(parsed)
-print(evaluate(parsed))
 
 def repl():
     while True:
-        parsed = parse(input(style.MAGENTA + 'scheme> '))
-        print(evaluate(parsed))
+        parsed = parse(input('scheme> '))
+        print(evaluate(parsed, standard_env))
 
-repl()
+
+
+if len(sys.argv) == 1:
+    repl()
+elif len(sys.argv) == 2:
+    f = open(sys.argv[1])
+    for line in f:
+        print(evaluate(parse(line)))
